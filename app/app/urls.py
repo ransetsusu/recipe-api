6cd45f5e220dd15639aa0be7d31af0b7fc65e65b
ransetsusu <brianluza.bl@gmail.com>
2021-05-14 00:55:30 +0800

    Add recipe endpoint

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls')),
 ]
